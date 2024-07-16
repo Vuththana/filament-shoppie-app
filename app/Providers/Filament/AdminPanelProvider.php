f3f503b4d10183a5439dfe57411a1efc3c36bda5
Vuththana <vuththanakeo69@gmail.com>
2024-07-16 07:55:33 +0700

    added more components and features

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -10,6 +10,7 @@ use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
 use Filament\Widgets;
+use Illuminate\Auth\Events\Logout;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
@@ -58,6 +59,7 @@ class AdminPanelProvider extends PanelProvider
             ->plugins([
                 FilamentEditProfilePlugin::make()
                     ->setIcon('heroicon-o-user')
+                    ->setNavigationGroup('Profile Setting')
                     ->shouldShowAvatarForm(
                         value: true,
                         directory: 'avatars', // image will be stored in 'storage/app/public/avatars
---
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -0,0 +1,1 @@
+<h1 class="font-semibold text-2xl flex items-center gap-2"><img src="../image/shoppie_logo.png" alt="Shoppie Logo" width="30px">Shoppie</h1>
